d641878c0fd598423f61d4e79895bb9ed4780246
cccarle <ce222qw@student.lnu.se>
2020-05-09 13:09:29 +0200

    added circel-layer, need to implement color based on timestamp

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -1081,6 +1081,7 @@ var initMap = function _initMap() {
       coordinates = covertToGoogleMapsCords(dataset_1); //createHeatmapLayer(coordinates)
       // createOverlayView()
 
+      createCircelsLayer(dataset_1);
       writePositionsToJSONByClick();
     });
   });
@@ -1102,72 +1103,7 @@ var calculateWeight = function calculateWeight(stdCoordinates) {
   }
 
   return crds;
-}; // const createOverlayView = () => {
-//   var overlay;
-//   USGSOverlay.prototype = new window.google.maps.OverlayView();
-//   var bounds = new window.google.maps.LatLngBounds(
-//     new window.google.maps.LatLng(56.65762137557452, 16.32347848127566),
-//     new window.google.maps.LatLng(56.65768329409489, 16.323813757402522));
-//   var srcImage = 'https://developers.google.com/maps/documentation/' +
-//     'javascript/examples/full/images/talkeetna.png';
-//   overlay = new USGSOverlay(bounds, srcImage, map);
-//   /* 
-//   Create Overlay-layer 
-//   TODO: add canvas to the overlay. 
-//   */
-//   function USGSOverlay(bounds, image, map) {
-//     this.bounds_ = bounds;
-//     this.image_ = image;
-//     this.map_ = map;
-//     this.div_ = null;
-//     this.setMap(map);
-//   }
-//   /**
-//    * onAdd is called when the map's panes are ready and the overlay has been
-//    * added to the map.
-//    */
-//   USGSOverlay.prototype.onAdd = function () {
-//     var div = document.createElement('div');
-//     div.style.borderStyle = 'none';
-//     div.style.borderWidth = '0px';
-//     div.style.position = 'absolute';
-//     // Create the img element and attach it to the div.
-//     var img = document.createElement('img');
-//     img.src = this.image_;
-//     img.style.width = '100%';
-//     img.style.height = '100%';
-//     img.style.position = 'absolute';
-//     div.appendChild(img);
-//     this.div_ = div;
-//     // Add the element to the "overlayLayer" pane.
-//     var panes = this.getPanes();
-//     panes.overlayLayer.appendChild(div);
-//   };
-//   USGSOverlay.prototype.draw = function () {
-//     // We use the south-west and north-east
-//     // coordinates of the overlay to peg it to the correct position and size.
-//     // To do this, we need to retrieve the projection from the overlay.
-//     var overlayProjection = this.getProjection();
-//     // Retrieve the south-west and north-east coordinates of this overlay
-//     // in LatLngs and convert them to pixel coordinates.
-//     // We'll use these coordinates to resize the div.
-//     var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
-//     var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
-//     // Resize the image's div to fit the indicated dimensions.
-//     var div = this.div_;
-//     div.style.left = sw.x + 'px';
-//     div.style.top = ne.y + 'px';
-//     div.style.width = (ne.x - sw.x) + 'px';
-//     div.style.height = (sw.y - ne.y) + 'px';
-//   };
-//   // The onRemove() method will be called automatically from the API if
-//   // we ever set the overlay's map property to 'null'.
-//   USGSOverlay.prototype.onRemove = function () {
-//     this.div_.parentNode.removeChild(this.div_);
-//     this.div_ = null;
-//   };
-// }
-
+};
 /* 
 Create Polygon with based on the given parameters. 
 */
@@ -1193,7 +1129,6 @@ Uses creatPolygon() to creates a new separate polygon for every array returned b
 var createPolygonLayer = function createPolygonLayer(coordinates) {
   var allCords = polygons(coordinates, 4)['10:00-10:59'];
   allCords.forEach(function (cordsArray) {
-    console.log(cordsArray);
     var polygon = creatPolygon(cordsArray, 'rgba(0, 0, 255, 1)', 1.2, 2, 'rgba(255, 0, 0, 1)', .55);
     polygon.setMap(map);
   });
@@ -1211,6 +1146,23 @@ var createHeatmapLayer = function createHeatmapLayer(coordinates) {
   heatmap.set('radius', heatmap.get('radius') ? radius : radius);
   var gradient = ['rgba(0, 0, 255, 0)', 'rgba(0, 0, 255, 1)', 'rgba(55, 0, 200, 1)', 'rgba(125, 0, 120, 1)', 'rgba(200, 0, 55, 1)', 'rgba(255, 0, 0, 1)'];
   heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+}; // create circels 
+
+
+var createCircelsLayer = function createCircelsLayer(coordinates) {
+  for (var coord in coordinates) {
+    var cityCircle = new window.google.maps.Circle({
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35,
+      map: map,
+      center: coordinates[coord].location,
+      radius: Math.sin(coordinates[coord].timestamp) * 2 // * 100 // Calculate searchRadus
+
+    });
+  }
 };
 
 var changeRadius = function changeRadius() {
@@ -1275,7 +1227,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53099" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63234" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
